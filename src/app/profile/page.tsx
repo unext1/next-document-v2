5ea23eb22323fb09767ca9b832bb122ca0099909
00andobe <00andobe@skola.boras.se>
2023-11-16 23:17:20 +0100

    css for profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,12 +4,21 @@ import LogoutBtn from "../components/logoutBtn";
 
 const ProfilePage = async () => {
   const session = await getServerSession(authOptions);
+
   return (
-    <div>
-      <h1>Hey {session?.user.name} !</h1>
-      <span>{JSON.stringify(session, null, 4)}</span>
-      <LogoutBtn />
-    </div>
+      <div className="bg-slate-900 p-6 rounded-xl shadow-md max-w-md w-full">
+        <h1 className="text-2xl font-bold mb-4">Hey {session?.user.name}!</h1>
+        <div className="mb-4">
+          <strong>Email:</strong> {session?.user.email}
+        </div>
+        <div className="mb-4">
+          <strong>User ID:</strong> {session?.user.id}
+        </div>
+        <div className="mb-4">
+          <strong>Role:</strong> {session?.user.role}
+        </div>
+        <LogoutBtn />
+      </div>
   );
 };
 
