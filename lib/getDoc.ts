6fde0704e049fd300a6990240a93c76f8036f4d3
laurynas <unextbusinessman@gmail.com>
2023-11-17 13:55:13 +0100

    permission changes

--- lib/getDoc.ts
+++ lib/getDoc.ts
@@ -1,12 +1,13 @@
 import { connect } from "@planetscale/database";
+
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { config } from "../db/config";
 import * as schema from "../db/schema";
 import { documents } from "../db/schema";
+
 import { authOptions } from "@/src/app/api/auth/[...nextauth]/route";
 import { eq, or, and } from "drizzle-orm";
 import { getServerSession } from "next-auth";
-
 export default async function GetDoc({ id }: { id: number }) {
   const session = await getServerSession(authOptions);
   const con = connect(config);
@@ -31,29 +32,12 @@ export default async function GetDoc({ id }: { id: number }) {
       },
     },
     where: or(
-      and(
-        eq(documents.id, id),
-        eq(documents.deleted, 0),
-        eq(documents.is_public, 1)
-      ),
-      and(
-        eq(documents.id, id),
-        eq(documents.deleted, 0),
-        or(
-          eq(documents.is_public, 0),
-          and(
-            eq(documents.is_public, 1),
-            eq(documents.user_id, Number(session?.user.id))
-          )
-        )
-      ),
+      and(eq(documents.id, id), eq(documents.deleted, 0)),
+      and(eq(documents.id, Number(session?.user.id)), eq(documents.deleted, 0)),
       and(
         eq(documents.id, id),
         eq(documents.deleted, 1),
-        and(
-          eq(documents.is_public, 1),
-          eq(documents.user_id, Number(session?.user.id))
-        )
+        eq(documents.user_id, Number(session?.user.id))
       )
     ),
   });
